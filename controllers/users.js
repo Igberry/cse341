8f010046218701bf31ca310de7363f8db6cd055c
Igberry <davidigberichuks@gmail.com>
2025-03-15 17:12:18 +0100

    commit

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
+// GET ALL USERS
 const getAll = async (req, res) => {
     //#swagger.tags = [Users]
     const result = mongodb.getDatabase().db().collection('users').find();
@@ -10,58 +11,84 @@ const getAll = async (req, res) => {
     });
 };
 
+// GET A SINGLE USER BY ID
 const getSingle = async (req, res) => {
     //#swagger.tags = [Users]
     const userId = new ObjectId(req.params.id);
-    const result = mongodb.getDatabase().db().collection('users').find({_id: userId});
-    result.toArray().then((users) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users[0]);
-    });
+    const result = await mongodb.getDatabase().db().collection('users').findOne({ _id: userId });
+
+    if (!result) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(result);
 };
 
+// POST - CREATE A NEW USER
 const createUser = async (req, res) => {
     //#swagger.tags = [Users]
-    const userId = new ObjectId (req.params.id);
     const user = {
         email: req.body.email,
         username: req.body.username,
         name: req.body.name,
-        ipaddress: req.body.ipaddress,
+        ipaddress: req.body.ipaddress
     };
-    const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occured while updating the user');
+
+    try {
+        const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
+        if (response.acknowledged) {
+            res.status(201).json({ message: "User created successfully", id: response.insertedId });
+        } else {
+            res.status(500).json({ error: "Failed to create user" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 };
 
+// PUT - UPDATE A USER
 const updateUser = async (req, res) => {
     //#swagger.tags = [Users]
-    const userId = new ObjectId (req.params.id);
+    const userId = new ObjectId(req.params.id);
     const user = {
         email: req.body.email,
         username: req.body.username,
         name: req.body.name,
-        ipaddress: req.body.ipaddress,
+        ipaddress: req.body.ipaddress
     };
-    const response = await mongodb.getDatabase().db().collection('users').replaceOne({_id: userId}, user);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occured while updating the user');
+
+    try {
+        const response = await mongodb.getDatabase().db().collection('users').updateOne(
+            { _id: userId },
+            { $set: user }
+        );
+
+        if (response.modifiedCount > 0) {
+            res.status(200).json({ message: "User updated successfully" });
+        } else {
+            res.status(404).json({ error: "User not found or no changes made" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 };
 
+// DELETE - REMOVE A SPECIFIC USER
 const deleteUser = async (req, res) => {
     //#swagger.tags = [Users]
-    const userId = new ObjectId (req.params.id);
-    const response = await mongodb.getDatabase().db().collection('users').deleteOne({_id: userId});
-    if (response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occured while updating the user');
+    const userId = new ObjectId(req.params.id);
+
+    try {
+        const response = await mongodb.getDatabase().db().collection('users').deleteOne({ _id: userId });
+
+        if (response.deletedCount > 0) {
+            res.status(200).json({ message: "User deleted successfully" });
+        } else {
+            res.status(404).json({ error: "User not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 };
 
